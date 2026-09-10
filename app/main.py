1a3be10f59682e2b97d3ee6ed856577f107872bd
Rohit84435 <rohit349a@gmail.com>
2026-09-10 09:12:41 +0530

    Added connection manager

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,status, HTTPException,Query , WebSocket
+from fastapi import FastAPI,status, HTTPException,Query , WebSocket, WebSocketDisconnect
 from pydantic import BaseModel, Field
 
 
@@ -16,6 +16,32 @@ class NotificationResponse(BaseModel):
     user_id: str
     title: str
     message: str
+    
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections = {}
+
+    def connect(self, user_id, websocket):
+        if user_id not in self.active_connections:
+            self.active_connections[user_id] = []
+        print("Connected - ","user_id ->",user_id," websocket ->",websocket)
+        self.active_connections[user_id].append(websocket)
+    
+    def disconnect(self, user_id, websocket):
+        self.active_connections[user_id].remove(websocket)
+        print("Disconnected - ","user_id ->",user_id," websocket ->",websocket)
+        if len(self.active_connections[user_id]) == 0:
+            del self.active_connections[user_id]
+    
+    async def send_to_user(self, user_id, message):
+        
+        if user_id in self.active_connections:
+            
+            for websocket in self.active_connections[user_id]:
+                await websocket.send_json(message)
+
+
+manager = ConnectionManager()          
 
 
 @app.get("/")
@@ -47,6 +73,8 @@ async def about_me():
 
 @app.post("/notifications", response_model=NotificationResponse,status_code=status.HTTP_201_CREATED)
 async def create_notification(notification: NotificationCreate):
+    
+    await manager.send_to_user(user_id=notification.user_id, message=notification.message)
     return {
         "id": 1,
         "user_id": notification.user_id,
@@ -66,10 +94,25 @@ async def get_notification(notification_id: int):
         "message" : "Your order has shipped"
     }
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket:WebSocket):
+@app.websocket("/ws/{user_id}")
+async def websocket_endpoint(websocket:WebSocket,user_id:str):
     await websocket.accept()
-    while True:
-        message = await websocket.receive_text()
-        print("This is connected message ->",message)
-        await websocket.send_text(message)
+    
+    manager.connect(user_id=user_id, websocket=websocket)
+    
+    await websocket.send_json({
+        "message":"Connected",
+        "user_id":user_id
+    })
+    
+    try:
+        while True:
+            message = await websocket.receive_text()
+            print("This is connected message ->",message)
+            await websocket.send_text(message)
+    
+    except WebSocketDisconnect:
+        print(f"{user_id} disconnected")
+    
+    finally:
+        manager.disconnect(user_id, websocket)
